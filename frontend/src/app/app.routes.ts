8f018d361eb1edc7bd4322f44e52b0c2deb2ff39
mbvgua <mbuguadouglas17@gmail.com>
2025-01-16 06:53:49 +0300

    get the basic frontend functionality working, except for the cart logic

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { Error404Component } from './components/error-404/error-404.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:DashboardComponent},
+    {path:'auth', children:
+        [
+            {path:'register', component:RegisterComponent},
+            {path:'login', component:LoginComponent},
+        ]
+    },
+    {path:'contact-us', component:ContactUsComponent},
+    {path:'**',component:Error404Component}
+];
