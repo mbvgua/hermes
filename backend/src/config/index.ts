0177a81006194527b97d286a81d05932c3b31c5f
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-08-11 13:05:00 +0300

    refactor(backend): make backend work as needed
    - had some trouble making the backend work after the long while,
      refactored it accordingly to make it work as needed now.

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,19 +1,23 @@
-import dotenv from 'dotenv'
-import { SqlConfig } from '../api/models/db.models'
+import dotenv from "dotenv";
+import { SqlConfig } from "../api/models/db.models";
+import path from "path";
 
-dotenv.config()
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-export const sqlConfig:SqlConfig = {
-    host:'localhost',
-    user:process.env.DB_USER,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    // from the docs
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0
-}
+export const sqlConfig: SqlConfig = {
+  host: "localhost",
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  // from the docs
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+};
+
+console.log(process.env.DB_NAME);
+
