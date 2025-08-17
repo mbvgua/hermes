0a4d4faf9bf529b7fb7ff4bfd767043a64dd2ba0
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-08-17 07:58:21 +0300

    feat(backend): build the getUser endpoint
    - make the getUser endpoint work, allowing a user to view their profile
      basically. Integration with the update endpoint later on will allow
      them to update it easily.
    - also changed the user validators making it more detailed and precise
      in capturing errors, and returning them.
    - formatted the auth.routes.ts file

--- backend/src/api/controllers/auth.controllers.ts
+++ backend/src/api/controllers/auth.controllers.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { logger } from "../../config/winston.config";
 import bcrypt from "bcrypt";
 
 import { pool } from "./../../config/db.config";
@@ -10,10 +11,11 @@ import {
 } from "../validators/users.validators";
 import { UserRoles, Users } from "../models/users.models";
 import { validationHelper } from "../helpers/validator.helpers";
-import { logger } from "../../config/winston.config";
 
-// register new users into system
 export async function registerUser(request: Request, response: Response) {
+  /*
+   * register new users into system
+   */
   const { username, email, password } = request.body;
   const role = UserRoles.Customer;
 
@@ -82,8 +84,10 @@ export async function registerUser(request: Request, response: Response) {
   }
 }
 
-// login user
 export async function loginUser(request: Request, response: Response) {
+  /*
+   * login existing users into the system
+   */
   const { username, password } = request.body;
 
   try {
@@ -194,116 +198,64 @@ export async function loginUser(request: Request, response: Response) {
   }
 }
 
-// get users
-export async function getUsers(request: Request, response: Response) {
+export async function getUserById(request: Request, response: Response) {
+  /*
+   * get user by their id, i.e user profile
+   * else return an error
+   */
+  const user_id = parseInt(request.query.id as string);
+
   try {
-    const [rows, fields] = await pool.query(
-      `SELECT * FROM users WHERE isDeleted=0;`,
+    const connection = await pool.getConnection();
+    const [rows] = await connection.query(
+      "SELECT * FROM users WHERE id=? AND is_deleted=0;",
+      [user_id],
     );
-    const users = rows as Array<Users[]>;
-    if (users) {
-      return response.status(200).send(users);
-    } else {
-      return response.status(401).json({
-        error: `There are currently no users in your database. Try again later?`,
+    const user_making_request = rows as Array<Users>;
+
+    if (user_making_request.length > 0) {
+      //log action
+      logger.log({
+        level: "info",
+        message: `${user_making_request[0].username} viewed their profile`,
+        data: {
+          user: {
+            username: user_making_request[0].username,
+            password: user_making_request[0].password,
+          },
+        },
       });
-    }
-  } catch (error) {
-    logger.log({
-      level: "error",
-      message: "Internal server error occurred",
-      data: { error },
-    });
 
-    return response.status(500).json({
-      code: 500,
-      status: "error",
-      message: "Internal server error",
-      data: { error },
-      metadata: null,
-    });
-  }
-}
-
-// update user
-export async function updateUser(
-  request: Request<{ id: string }>,
-  response: Response,
-) {
-  const id = request.params.id;
-  const { username, email, password } = request.body;
-  const { error } = updateUserSchema.validate(request.body);
-  try {
-    if (error) {
-      return response.status(401).json({ error: error.details[0].message });
-    } else {
-      const [rows, fields] = await pool.query(
-        `SELECT * FROM users
-                WHERE id='${id}
-                AND isDeleted=0;'`,
-      );
-      const [user] = rows as Array<Users>;
-      if (user) {
-        const hashedPassword = await bcrypt.hash(password, 9);
-        await pool.query(
-          `UPDATE users SET
-                        username='${username}',
-                        email='${email}',
-                        password='${hashedPassword}'
-                    WHERE id='${user.id}'
-                    AND isDeleted=0;`,
-        );
-        return response.status(200).json({
-          success:
-            "Congratulations! Your details have been successfully updated.",
-        });
-      } else {
-        return response
-          .status(401)
-          .json({ error: `User does not exist. Try again?` });
-      }
-    }
-  } catch (error) {
-    logger.log({
-      level: "error",
-      message: "Internal server error occurred",
-      data: { error },
-    });
-
-    return response.status(500).json({
-      code: 500,
-      status: "error",
-      message: "Internal server error",
-      data: { error },
-      metadata: null,
-    });
-  }
-}
-
-// delete user
-export async function deleteUser(
-  request: Request<{ id: string }>,
-  response: Response,
-) {
-  const id = request.params.id;
-  try {
-    const [rows, fields] = await pool.query(
-      `SELECT * FROM users
-            WHERE id='${id}'
-            AND isDeleted=0;`,
-    );
-    const [user] = rows as Array<Users>;
-    if (user) {
-      const [rows, fields] = await pool.query(
-        `UPDATE users SET isDeleted=1 WHERE id = '${id}';`,
-      );
+      //return profile
       return response.status(200).json({
-        success: `Congratulations! You have successfully deleted you account.`,
+        code: 200,
+        status: "success",
+        message: `Successfully retrieved ${user_making_request[0].username}'s profile`,
+        data: {
+          user: {
+            id: user_id,
+            username: user_making_request[0].username,
+            email: user_making_request[0].email,
+            role: user_making_request[0].role,
+          },
+        },
       });
     } else {
-      return response
-        .status(401)
-        .json({ error: `You do not have an account. Try again?` });
+      //user does not exist
+      //TODO: find out why this does NOT run, it skips right to the catch block
+      logger.log({
+        level: "error",
+        message: `Tried viewing profile of ${user_making_request[0].id} which does not exist`,
+        data: null,
+      });
+
+      return response.status(404).json({
+        code: 404,
+        status: "error",
+        message: "User not found",
+        data: null,
+        metadata: null,
+      });
     }
   } catch (error) {
     logger.log({
@@ -321,3 +273,123 @@ export async function deleteUser(
     });
   }
 }
+
+export async function getUsers(request: Request, response: Response) {
+  /*
+   * get users in system
+   * has pagination with limit-offset
+   */
+  try {
+  } catch (error) {
+    logger.log({
+      level: "error",
+      message: "Internal server error occurred",
+      data: { error },
+    });
+
+    return response.status(500).json({
+      code: 500,
+      status: "error",
+      message: "Internal server error",
+      data: { error },
+      metadata: null,
+    });
+  }
+}
+
+//// update user
+//export async function updateUser(
+//  request: Request<{ id: string }>,
+//  response: Response,
+//) {
+//  const id = request.params.id;
+//  const { username, email, password } = request.body;
+//  const { error } = updateUserSchema.validate(request.body);
+//  try {
+//    if (error) {
+//      return response.status(401).json({ error: error.details[0].message });
+//    } else {
+//      const [rows, fields] = await pool.query(
+//        `SELECT * FROM users
+//                WHERE id='${id}
+//                AND isDeleted=0;'`,
+//      );
+//      const [user] = rows as Array<Users>;
+//      if (user) {
+//        const hashedPassword = await bcrypt.hash(password, 9);
+//        await pool.query(
+//          `UPDATE users SET
+//                        username='${username}',
+//                        email='${email}',
+//                        password='${hashedPassword}'
+//                    WHERE id='${user.id}'
+//                    AND isDeleted=0;`,
+//        );
+//        return response.status(200).json({
+//          success:
+//            "Congratulations! Your details have been successfully updated.",
+//        });
+//      } else {
+//        return response
+//          .status(401)
+//          .json({ error: `User does not exist. Try again?` });
+//      }
+//    }
+//  } catch (error) {
+//    logger.log({
+//      level: "error",
+//      message: "Internal server error occurred",
+//      data: { error },
+//    });
+//
+//    return response.status(500).json({
+//      code: 500,
+//      status: "error",
+//      message: "Internal server error",
+//      data: { error },
+//      metadata: null,
+//    });
+//  }
+//}
+//
+//// delete user
+//export async function deleteUser(
+//  request: Request<{ id: string }>,
+//  response: Response,
+//) {
+//  const id = request.params.id;
+//  try {
+//    const [rows, fields] = await pool.query(
+//      `SELECT * FROM users
+//            WHERE id='${id}'
+//            AND isDeleted=0;`,
+//    );
+//    const [user] = rows as Array<Users>;
+//    if (user) {
+//      const [rows, fields] = await pool.query(
+//        `UPDATE users SET isDeleted=1 WHERE id = '${id}';`,
+//      );
+//      return response.status(200).json({
+//        success: `Congratulations! You have successfully deleted you account.`,
+//      });
+//    } else {
+//      return response
+//        .status(401)
+//        .json({ error: `You do not have an account. Try again?` });
+//    }
+//  } catch (error) {
+//    logger.log({
+//      level: "error",
+//      message: "Internal server error occurred",
+//      data: { error },
+//    });
+//
+//    return response.status(500).json({
+//      code: 500,
+//      status: "error",
+//      message: "Internal server error",
+//      data: { error },
+//      metadata: null,
+//    });
+//  }
+//}
