fd7b92300f77fd2e60edddd95b7afc97bf2bf453
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-08-18 16:49:36 +0300

    feat(backend): add the updateUser endpoint
    - integrate the update user endpoint to utilize both middleware and
      logging. All added necessary validation and error checks to mak eit
      better generally

--- backend/src/api/routes/users.routes.ts
+++ backend/src/api/routes/users.routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { getUserById, getUsers } from "../controllers/auth.controllers";
+import {
+  getUserById,
+  getUsers,
+  updateUser,
+} from "../controllers/users.controllers";
 import { isAdmin, verifyToken } from "../middlewares/auth.middleware";
 
 const userRouter = express.Router();
 
 userRouter.get("/get-user", verifyToken, getUserById);
 userRouter.get("/get-users", verifyToken, isAdmin, getUsers);
+userRouter.patch("/update", verifyToken, updateUser);
 
 export default userRouter;
