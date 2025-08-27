35c933deb75e83ffb888e739d069ceda6759f956
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-08-27 19:07:56 +0300

    feat(frontend): add the profile page
    - build and add the profile page to display user details dynamically
      from the backend

--- frontend/src/app/components/dashboard/profile/profile.spec.ts
+++ frontend/src/app/components/dashboard/profile/profile.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Users } from './users';
+import { Profile } from './profile';
 
-describe('Users', () => {
-  let component: Users;
-  let fixture: ComponentFixture<Users>;
+describe('Profile', () => {
+  let component: Profile;
+  let fixture: ComponentFixture<Profile>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Users]
+      imports: [Profile]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Users);
+    fixture = TestBed.createComponent(Profile);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
