dd4c4d792277363d8a4c74effc57fe93b93a1946
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-09-19 21:52:52 +0300

    feat(oauth): add a new way of db interaction
    - Ayyoo! realized how to interact with the db staright from within my
      app, entirely eliminating any need for models or the ORM!! FINALLY!
    - Though you could add it, I find that it adds an unecessary layer of
      complexity within the app. I have added the models in these repos:
        - https://github.com/mbvgua/oauth-primer/blob/main/website/models.py
        - https://github.com/mbvgua/iFarm/blob/main/3.Building-the-Website/iFarm/website/models.py
        - https://github.com/mbvgua/web-sandbox/blob/main/01.genesis/website/models.py
      and found that as the number of tables increases, complexity of the
      models and how they relate to each other also increases.
    - Will try working with databases in flask with this method hence forth

--- oauthServices/webapp/__init__.py
+++ oauthServices/webapp/__init__.py
@@ -1,11 +1,13 @@
 # python standard libraries
 import os
+import sqlite3
 
 # 3rd party libs
 from flask import Flask
 from dotenv import load_dotenv
 
 # local imports
+from .db import init_db_command
 
 load_dotenv()
 
@@ -18,6 +20,14 @@ def create_app():
     # Interesting way to put it:
     # https://realpython.com/flask-database/#hide-your-secrets
     app.config.from_prefixed_env()
+
+    # register db in app
+    from . import db
+
+    try:
+        db.init_app(app)
+    except sqlite3.OperationalError as e:
+        print(f"An error occurred while creating the database: {e}")
 
     print(f"Current Environment: {os.getenv('ENVIRONMENT')}")
     print(f"Using database: {app.config.get('DATABASE')}")
--- oauthServices/webapp/db.py
+++ oauthServices/webapp/db.py
@@ -1,3 +1,68 @@
 # python standard libs
+import sqlite3
+
 # 3rd party libs
+from flask import current_app, g
+from flask.cli import with_appcontext
+import click
+
 # local imports
+
+
+def get_db():
+    """
+    returns a database connection. either returning an existing one
+    or establishing a new one
+    """
+
+    # g -> provides a global namespace you use as temp storage when user
+    # makes a request. Each request has its own g object, that resets at the
+    # end of the request.
+    if "db" not in g:
+        # connect to db in .env file with sqlite3
+        g.db = sqlite3.connect(
+            current_app.config["DATABASE"],
+            detect_types=sqlite3.PARSE_DECLTYPES,
+        )
+        # interact with db columns by their names. Like a dictionary
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    """
+    close the db connection.
+    e -> Error. Though not used, it allows for error handling
+    """
+    # retrieve and remove db connection fom the "g" object. This
+    # means any active connections are closes eventually
+    # NOTE: 1 over express: I have to manually connect then later 
+    # connection.release() everytime
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+
+
+def init_app(app):
+    """
+    core logic for db initialization
+    add init_db_command() as a CLI command with click
+    """
+    # close db when app context closes. the app context in flask is 
+    # created in each request.
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
+@click.command("init-db")
+def init_db_command():
+    db = get_db()
+
+    # read and execute commands from "db.sql" file, creating
+    # tables and setting up the schema
+    with current_app.open_resource("database/db.sql") as f:
+        db.executescript(f.read().decode("utf-8"))
+
+    click.echo("You successfully initialized the database")
