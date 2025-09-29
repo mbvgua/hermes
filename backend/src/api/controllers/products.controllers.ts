f3b85f10c087af59610ab27562406de10f95e557
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-09-29 21:22:16 +0300

    feat(product): add the GET products module
    - implement the endpoints needed for the GET functionality in the
      products, which includes:
      - getProducts
      - getProductById
      - getProductByCategory
    - no pagination had been implemented yet, will do this gradually.

--- backend/src/api/controllers/products.controllers.ts
+++ backend/src/api/controllers/products.controllers.ts
@@ -38,9 +38,6 @@ export async function addProduct(request: Request, response: Response) {
 
       //release db connection
       connection.release();
-      console.log(
-        `Product ${name} of ${id} has succesfully been added to the system`,
-      );
 
       //log information
       logger.log({
@@ -90,79 +87,285 @@ export async function addProduct(request: Request, response: Response) {
   }
 }
 
-// // get product by id
-// export async function getProductById(request:Request<{id:string}>,response:Response){
-//     const id = request.params.id
-//     try {
-//         const [rows,fields] = await pool.query(
-//             `SELECT * FROM products
-//             WHERE id='${id}'
-//             AND isDeleted=0;`
-//         )
-//         const [product] = rows as Array<Products>
-//         if(product){
-//             return response.status(200).send(product)
-//         } else {
-//             return response.status(401).json({error:`Product does not exist. Try again?`})
-//         }
-//     } catch(error){
-//         console.error('An error occured: ',error)
-//         return response.status(500).json({error:error})
-//     }
-// }
-//
-// // get products
-// export async function getProducts(request:Request,response:Response){
-//     try{
-//         const [rows,fields] = await pool.query(
-//             `SELECT * FROM products
-//             WHERE isDeleted=0;`
-//         )
-//         const products = rows as Array<Products[]>
-//         if(products.length != 0){
-//             return response.status(200).send(products)
-//         } else {
-//             return response.status(401).json({error:`There are currently no products. Try again later?`})
-//         }
-//     } catch(error) {
-//         console.error('An error occured: ',error)
-//         return response.status(500).json({error:error})
-//     }
-// }
-//
+export async function getProductById(
+  request: Request<{ id: string }>,
+  response: Response,
+) {
+  /*
+   * get a specific product by its id
+   */
+  const id = request.params.id;
+  try {
+    const connection = await pool.getConnection();
+    const [rows]: any = await connection.execute("CALL getProductById(?);", [
+      id,
+    ]);
+    const [product] = rows[0] as Array<IProducts>;
+
+    // if product exists
+    if (product) {
+      //log the action
+      logger.log({
+        level: "info",
+        message: `Succesfully retrieved the ${product.name} of id:${product.id} from the database`,
+        data: {
+          product: {
+            id: product.id,
+            name: product.name,
+            category: product.category,
+          },
+        },
+      });
+
+      //return success response
+      return response.status(200).json({
+        code: 200,
+        status: "success",
+        message: `Succesfully retrieved the ${product.name} of id:${product.id} from the database`,
+        data: {
+          product: {
+            id: product.id,
+            name: product.name,
+            category: product.category,
+          },
+        },
+        metadata: null,
+      });
+    }
+    // else if no product return and log error
+    logger.log({
+      level: "error",
+      message: `Tried fetching product of id: ${id} which does not exist.`,
+      data: null,
+    });
+
+    return response.status(401).json({
+      code: 401,
+      status: "error",
+      message: `Product of id:${id} does not exist in the database`,
+      data: null,
+      metadata: null,
+    });
+  } catch (error) {
+    // log the error
+    logger.log({
+      level: "error",
+      message: "Internal server error occurred",
+      data: { error },
+    });
+
+    // return respnse
+    return response.status(500).json({
+      code: 500,
+      status: "error",
+      message: "Internal server error",
+      data: { error },
+      metadata: null,
+    });
+  }
+}
+
+export async function getProductsByCategory(
+  request: Request,
+  response: Response,
+) {
+  /*
+   * get products from the db with their categories
+   * not a request, but instead in the URL, say 123/get/eectronics
+   */
+  const category = request.query.category as string;
+  try {
+    const connection = await pool.getConnection();
+    const [rows]: any = await connection.execute(
+      "CALL getProductByCategory(?);",
+      [category],
+    );
+    const [products] = rows[0] as Array<IProducts>;
+
+    if (products) {
+      // log request
+      logger.log({
+        level: "info",
+        message: `Succesfully retrieved items under the ${category} category.`,
+        data: {
+          products: {
+            // begin pagination to return this better
+          },
+        },
+      });
+      // return response
+      return response.status(200).json({
+        code: 200,
+        status: "success",
+        message:
+          "Succesfully retrieved products under the ${category} category",
+        data: {
+          products: {
+            // begin pagination to return this better
+          },
+        },
+      });
+    }
+    // log request
+    logger.log({
+      level: "error",
+      message: `Error fetching products in the ${category} category.`,
+      data: {
+        products: {
+          // begin pagination to return this better
+        },
+      },
+    });
+    // return response
+    return response.status(404).json({
+      code: 404,
+      status: "error",
+      message: `No products found in the ${category} category`,
+      data: {
+        products: {
+          // begin pagination to return this better
+        },
+      },
+      metadata: null,
+    });
+  } catch (error) {
+    // log the error
+    logger.log({
+      level: "error",
+      message: "Internal server error occurred",
+      data: { error },
+    });
+
+    // return respnse
+    return response.status(500).json({
+      code: 500,
+      status: "error",
+      message: "Internal server error",
+      data: { error },
+      metadata: null,
+    });
+  }
+}
+
+export async function getProducts(request: Request, response: Response) {
+  /*
+   * get all products from the system
+   */
+  try {
+    const connection = await pool.getConnection();
+    const [rows]: any = await connection.execute(`CALL getProducts();`);
+    const [products] = rows[0] as Array<IProducts>;
+    connection.release();
+
+    if (products) {
+      //log action
+      logger.log({
+        level: "info",
+        message: "Succesfully retrieved all products in the system",
+        data: {
+          products: {
+            // begin pagination to return this better
+          },
+        },
+      });
+      //return response
+      return response.status(200).json({
+        code: 200,
+        status: "error",
+        message: "Succesfully retrieved all products in the system",
+        data: {
+          products: {
+            // begin pagination to return this better
+          },
+        },
+        metadata: null,
+      });
+    }
+    // log request
+    logger.log({
+      level: "error",
+      message: `Error fetching products in the system`,
+      data: {
+        products: {
+          // begin pagination to return this better
+        },
+      },
+    });
+    // return response
+    return response.status(404).json({
+      code: 404,
+      status: "error",
+      message: `No products found in the system. Try again later`,
+      data: {
+        products: {
+          // begin pagination to return this better
+        },
+      },
+      metadata: null,
+    });
+  } catch (error) {
+    // log the error
+    logger.log({
+      level: "error",
+      message: "Internal server error occurred",
+      data: { error },
+    });
+
+    // return respnse
+    return response.status(500).json({
+      code: 500,
+      status: "error",
+      message: "Internal server error",
+      data: { error },
+      metadata: null,
+    });
+  }
+}
+
 // // update product
-// export async function updateProduct(request:Request<{id:string}>,response:Response){
-//     const id = request.params.id
-//     const{name,description,image,price,inStock} = request.body
-//     const {error} = updateProductSchema.validate(request.body)
-//     try{
-//         if(error){
-//             return response.status(401).json({error:error.details[0].message})
-//         } else {
-//             const [rows,fields] = await pool.query(
-//                 `SELECT * FROM products WHERE id='${id}';`
-//             )
-//             const [product] = rows as Array<Products>
-//             if(product){
-//                 await pool.query(
-//                     `UPDATE products SET
+// export async function updateProduct(
+//   request: Request<{ id: string }>,
+//   response: Response,
+// ) {
+//   const id = request.params.id;
+//   const { name, description, image, price, inStock } = request.body;
+//   const { error } = updateProductSchema.validate(request.body);
+//   try {
+//     if (error) {
+//       return response.status(401).json({ error: error.details[0].message });
+//     } else {
+//       const [rows, fields] = await pool.query(
+//         `SELECT * FROM products WHERE id='${id}';`,
+//       );
+//       const [product] = rows as Array<Products>;
+//       if (product) {
+//         await pool.query(
+//           `UPDATE products SET
 //                     name='${name}',
 //                     description='${description}',
 //                     image='${image}',
 //                     price='${price}',
 //                     inStock='${inStock}'
 //                     WHERE id='${id}'
-//                     AND isDeleted=0;`
-//                 )
-//                 return response.status(200).json({success:`You have succesfully updated the ${product.name}!`})
-//             } else {
-//                 return response.status(401).json({error:`The product does not seem to exist. Try again later?`})
-//             }
-//         }
-//     } catch(error) {
-//         console.error('An error occurred: ',error)
-//         return response.status(500).json({error:error})
+//                     AND isDeleted=0;`,
+//         );
+//         return response
+//           .status(200)
+//           .json({
+//             success: `You have succesfully updated the ${product.name}!`,
+//           });
+//       } else {
+//         return response
+//           .status(401)
+//           .json({
+//             error: `The product does not seem to exist. Try again later?`,
+//           });
+//       }
 //     }
+//   } catch (error) {
+//     console.error("An error occurred: ", error);
+//     return response.status(500).json({ error: error });
+//   }
 // }
 //
 // // delete  product
